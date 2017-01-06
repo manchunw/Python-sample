46e7ef2993ee964d168bc37e8b87c28e54ad339e
Kelvin <manchunw@gmail.com>
2017-01-06 12:18:31 +0800

    Python get more contents and draw qq plot

--- py-machine-learn/3-qq-plot.py
+++ py-machine-learn/3-qq-plot.py
@@ -1,7 +1,10 @@
 from urllib.request import urlopen
 import sys
 import numpy as np
+import pylab
+import scipy.stats as stats
 
+# import data file
 target_url = ("https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data")
 
 data = urlopen(target_url)
@@ -18,11 +21,14 @@ col = 3
 colData = []
 for row in xList:
 	colData.append(float(row[col]))
+
+# show mean and sd
 colArray = np.array(colData)
 colMean = np.mean(colArray)
 colsd = np.std(colArray)
 sys.stdout.write("Mean = " + '\t' + str(colMean) + '\t\t' + "Standard Deviation = " + '\t' + str(colsd) + '\n')
 
+# show 4 percentiles
 ntiles = 4
 percentBdry = []
 for i in range(ntiles+1):
@@ -31,6 +37,7 @@ sys.stdout.write("\nBoundaries for 4 Equal Percentiles\n")
 print(percentBdry)
 sys.stdout.write("\n")
 
+# show 10 percentiles
 ntiles = 10
 percentBdry = []
 for i in range(ntiles+1):
@@ -39,6 +46,7 @@ sys.stdout.write("\nBoundaries for 10 Equal Percentiles\n")
 print(percentBdry)
 sys.stdout.write("\n")
 
+# show unique label values
 col = 60
 colData = []
 for row in xList:
@@ -47,6 +55,7 @@ unique = set(colData)
 sys.stdout.write("Unique Label Values\n")
 print(unique)
 
+# show count
 catDict = dict(zip(list(unique), range(len(unique))))
 catCount = [0]*2
 for elt in colData:
@@ -54,6 +63,15 @@ for elt in colData:
 sys.stdout.write("\nCounts for Each Value of Categorical Label \n")
 print(list(unique))
 print(catCount)
+
+labels = []
+col = 3
+colData = []
+for row in xList:
+	colData.append(float(row[col]))
+
+stats.probplot(colData, dist="norm", plot=pylab)
+pylab.show()
 
 sys.stdout.write("Number of Rows of Data = " + str(len(xList)) + "\n")
 sys.stdout.write("Number of Rows of Data = " + str(len(xList[1])) + "\n")
