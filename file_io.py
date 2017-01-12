f0f362d89ce9bcac4160379279442e6c2c2611fd
Kelvin <manchunw@gmail.com>
2017-01-12 12:22:52 +0800

    Update File I/O

--- file_io.py
+++ file_io.py
@@ -7,3 +7,30 @@ for item in my_list:
     f.write(str(item) + "\n")
 
 f.close()
+
+
+
+
+my_file = open("text.txt", "r")
+print my_file.readline()
+print my_file.readline()
+print my_file.readline()
+my_file.close()
+
+
+
+# Open the file for reading
+read_file = open("text.txt", "r")
+# Use a second file handler to open the file for writing
+write_file = open("text.txt", "w")
+# Write to the file
+write_file.write("Not closing files is VERY BAD.")
+write_file.close()
+# Try to read from the file
+print read_file.read()
+read_file.close()
+
+
+
+with open("text.txt", "w") as textfile:
+	textfile.write("Success!")
