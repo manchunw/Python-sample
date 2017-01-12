0f723a5b34f1f004ff352bd5b7c6d90896e67f0e
Kelvin <manchunw@gmail.com>
2017-01-12 09:54:00 +0800

    Python class inheritance and representation

--- class.py
+++ class.py
@@ -89,3 +89,14 @@ class Equilateral(Triangle):
         self.angle1 = self.angle
         self.angle2 = self.angle
         self.angle3 = self.angle
+
+class Point3D(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    def __repr__(self):
+        return "(%d, %d, %d)" % (self.x, self.y, self.z)
+
+my_point = Point3D(1, 2, 3)
+print my_point
